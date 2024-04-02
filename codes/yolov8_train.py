e0f9d6c55f9a85b337cb37ca2c11523faddb80aa
Keddmon <dh03219@naver.com>
2024-04-02 22:18:30 +0900

    feat. something added

--- codes/yolov8_train.py
+++ codes/yolov8_train.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
 
     # 모델 불러오기
     # model = YOLO('models\\TOP&BOTTOM.pt')
-    model = YOLO('yolov8n.pt')
+    model = YOLO('models\\TOP&BOTTOM.pt')
 
     # 모델 학습
-    train = model.train(data = 'selectlabeling\\dataset.yaml', epochs=30)
+    train = model.train(data = 'new_label_data\\dataset.yaml', epochs=50)
