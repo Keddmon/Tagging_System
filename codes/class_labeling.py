8e78427678662604d841acd87e2126bfaaa09b91
Keddmon <dh03219@naver.com>
2024-04-04 15:29:52 +0900

    feat. modified add classes

--- codes/class_labeling.py
+++ codes/class_labeling.py
@@ -1,71 +1,94 @@
 from PIL import Image
 from ultralytics import YOLO
 import os
-import shutil # 이미지 복사
+import shutil
 
+#################### 파일 및 폴더 생성 ####################
 
+# TOP: 0, BOTTOM: 1
+class_mapping = []
 
 # 사전 모델 불러오기 (상하의)
-model = YOLO('models\\TOP&BOTTOM.pt')
+model = YOLO('models//TOP&BOTTOM.pt')
+
+# 실패한 이미지 갯수
+failed_image_count = 0
 
 # 폴더 생성
 if not os.path.exists('new_label_data'):
     os.makedirs('new_label_data')
 
 # 라벨 파일 폴더 생성
-if not os.path.exists('new_label_data\\labels'):
-    os.makedirs('new_label_data\\labels')
+if not os.path.exists('new_label_data//labels'):
+    os.makedirs('new_label_data//labels')
+# 라벨 파일 폴더 주소
+labels_dir = 'new_label_data//labels'
 
 # 이미지 파일 폴더 생성
-if not os.path.exists('new_label_data\\images'):
-    os.makedirs('new_label_data\\images')
+if not os.path.exists('new_label_data//images'):
+    os.makedirs('new_label_data//images')
+# 이미지 파일 폴더 주소
+target_images_dir = 'new_label_data//images'  # 복사할 폴더
+source_images_dir = 'test_images'  # 원본 이미지 파일 폴더
 
-# 라벨 파일 폴더
-labels_dir = 'new_label_data\\labels'
+#######################################################
 
-# 이미지 파일 폴더
-target_images_dir = 'new_label_data\\images' # 복사할 폴더
-source_images_dir = 'test_images' # 원본 이미지 파일 폴더
-
+# 원본 이미지 파일 폴더의 이미지들을 복사할 폴더로 복사
+image_files = sorted([filename for filename in os.listdir(source_images_dir) if filename.endswith('.png') or filename.endswith('.jpg')])
 for filename in os.listdir(source_images_dir):
-    if filename.endswith('.png') or filename.endswith('jpg'):
-        source_path = os.path.join(source_images_dir, filename)
-        target_path = os.path.join(target_images_dir, filename)
-        shutil.copyfile(source_path, target_path)
-
-images_paths = [os.path.join(target_images_dir, img) for img in os.listdir(target_images_dir) if img.endswith(".png") or img.endswith("jpg")]
+    source_path = os.path.join(source_images_dir, filename)
+    target_path = os.path.join(target_images_dir, filename)
+    shutil.copyfile(source_path, target_path)
 
 # 클래스 파일 생성
-with open(f"{labels_dir}\\classes.txt", "w") as f:
+with open(f"{labels_dir}//classes.txt", "w") as f:
     for name in model.names.values():
         f.write(f"{name}\n")
+        class_mapping.append(name)
+
+##################################################
 
 # 객체 탐지 결과
-results = model(images_paths)
+results = model([os.path.join(target_images_dir, img) for img in image_files])
+
+#################### 클래스 추가 ####################
+
+# 추가할 클래스 집합 초기화
+additional_classes = []
 
 #################### 각 이미지에 대한 라벨 데이터 생성 및 정규화 ####################
-# 각 이미지 파일
-for i in range(len(results)):
 
-    # 탐지된 객체 값 구하기
-    objects = results[i].boxes.xyxy
+# 이미지와 함께 각각의 이미지 내에 존재하는 패션 아이템의 상의와 하의 종류 입력 받기 (테스트)
+for image_path, result in zip([os.path.join(target_images_dir, img) for img in image_files], results):
+    print(f"Image: {image_path}")
 
-    # confidence, class_id 구하기
-    confidence = results[i].boxes.conf
-    class_id = results[i].boxes.cls
+    # 이미지당 탐지된 객체의 개수 확인
+    objects = result.boxes.xyxy  # 수정된 부분
+    if len(objects) != 2:
+        print("실패, 객체가 3개 이상 탐지됨.")
+        failed_image_count += 1
+        continue
 
-    # 이미지 이름 구하기
-    images_name = os.path.splitext(os.path.basename(images_paths[i]))[0]
+    top_category = input("Enter the TOP category: ")
+    bottom_category = input("Enter the bottom category: ")
 
-    # 라벨 데이터 작성
-    with open(f"{labels_dir}\\{images_name}.txt", "w") as f:
-        for j in range(len(objects)):
-            x1, y1, x2, y2 = objects[j]
-            conf = confidence[j]
-            cls_id = int(class_id[j])
+    # 새로운 클래스 추가
+    if top_category not in class_mapping:
+        class_mapping.append(top_category)
+        additional_classes.append(top_category)
+    if bottom_category not in class_mapping:
+        class_mapping.append(bottom_category)
+        additional_classes.append(bottom_category)
+
+
+    # 이미지당 객체에 대한 라벨 데이터 작성
+    images_name = os.path.splitext(os.path.basename(image_path))[0]
+    with open(f"{labels_dir}//{images_name}.txt", "w") as f:
+        for obj in objects: 
+            x1, y1, x2, y2 = obj
 
             # 이미지 너비, 높이 구하기
-            img = Image.open(images_paths[i])
+            img = Image.open(image_path)
             img_width, img_height = img.size
 
             # bounding box의 중심 좌표, 너비, 높이 계산
@@ -74,16 +97,21 @@ for i in range(len(results)):
             width = round(((x2 - x1) / img_width).item(), 6)
             height = round(((y2 - y1) / img_height).item(), 6)
 
-            f.write(f"{cls_id} {x_center} {y_center} {width} {height}\n")
-#################################################################################
+            # 라벨 데이터 작성
+            images_name = os.path.splitext(os.path.basename(image_path))[0]
+            with open(f"{labels_dir}//{images_name}.txt", "w") as f:
+                f.write(f"{class_mapping.index(top_category)} {x_center} {y_center} {width} {height}\n")
+                f.write(f"{class_mapping.index(bottom_category)} {x_center} {y_center} {width} {height}\n")
 
-# 설정 파일 생성
-if not os.path.exists('new_label_data\\dataset.yaml'):
-    # dataset.yaml 파일 생성 코드
-    f = open('new_label_data\\dataset.yaml', 'w')
+# 클래스 파일 업데이트
+with open(f"{labels_dir}//classes.txt", "a") as f:
+    for cls in additional_classes:
+        f.write(f"{cls}\n")
 
-with open("new_label_data\\dataset.yaml", "w") as f:
-    f.write(f"train: images\n")  # 학습 이미지 경로
-    f.write(f"val: images\n")  # 검증 이미지 경로
-    f.write(f"nc: {len(model.names)}\n")  # 클래스 개수
-    f.write(f"names: {list(model.names.values())}\n")  # 클래스 이름
+# dataset.yaml 파일 생성
+if not os.path.exists('new_label_data//dataset.yaml'):
+    with open("new_label_data//dataset.yaml", "w") as f:
+        f.write(f"train: images\n")  # 학습 이미지 경로
+        f.write(f"val: images\n")  # 검증 이미지 경로
+        f.write(f"nc: {len(model.names) + len(additional_classes)}\n")  # 클래스 개수
+        f.write(f"names: {list(model.names.values()) + additional_classes}\n")  # 클래스 이름
